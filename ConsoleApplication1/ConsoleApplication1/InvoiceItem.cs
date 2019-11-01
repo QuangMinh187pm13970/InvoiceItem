ba4dab37802e210cf7482a9582a2afc47ae76d12
QuangMinh187pm13970 <quangminhkt2000@gmail.com>
2019-11-01 11:54:41 +0700

    InvoiceItem

---
+++ ConsoleApplication1/ConsoleApplication1/InvoiceItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class InvoiceItem
+    {
+        private string id;
+        private string desc;
+        private int qty;
+        private double unitPrice;
+        public InvoiceItem(string id, string desc, int qty, double unitPrice)
+        {
+            this.id = id;
+            this.desc = desc;
+            this.qty = qty;
+            this.unitPrice = unitPrice;
+        }
+        public string getID()
+        {
+            return this.id;
+        }
+        public string getDesc()
+        {
+            return this.desc;
+        }
+        public int getQty()
+        {
+            return this.qty;
+        }
+        public double getUnitPrice()
+        {
+            return this.unitPrice;
+        }
+        public void setUnitPrice(double newUnitPrice)
+        {
+            this.unitPrice = newUnitPrice;
+        }
+        public double getTotal()
+        {
+            return this.unitPrice * this.qty;
+        }
+        public string toString()
+        {
+            return string.Format("InvoiceItem[id = {0}, desc = {1}, qty = {2}, unitPrice = {3}]", this.id, this.desc, this.qty, this.unitPrice);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InvoiceItem i = new InvoiceItem("300", "200", 2, 500);
+            Console.WriteLine(i.toString());
+            i.setUnitPrice(300);
+            Console.WriteLine(i.toString());
+            Console.WriteLine(i.getTotal());
+
+        }
+    }
+}
